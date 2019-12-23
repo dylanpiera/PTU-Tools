9fe6f4950abef57359706a14efa566b771f683a0
dylan@fampiera.nl <dylan@fampiera.nl>
2019-12-23 19:32:38 +0100

    Add Radzen

--- PTU-Tools-Web/Startup.cs
+++ PTU-Tools-Web/Startup.cs
@@ -39,7 +39,11 @@ namespace PTU_Tools_Web
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            services.AddSingleton<WeatherForecastService>();
+
+
+
+            ///TODO: Update this to be the proper class name
+            services.AddSingleton<Google_Sheets_Integration.Class1>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
